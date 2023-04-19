53c4a94a5ce03d5dac2ae891b31ba898b6969696
fcjdm <fcjdom@gmail.com>
2023-04-19 21:48:22 +0200

    clean

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDtjlSKCakftogDK0XVeOwv0e4iI2R4Jh8",
+    authDomain: "gymfy-e76e2.firebaseapp.com",
+    projectId: "gymfy-e76e2",
+    storageBucket: "gymfy-e76e2.appspot.com",
+    messagingSenderId: "1098437118781",
+    appId: "1:1098437118781:web:2c2ce0413c17adfbe81354"
+};
+
