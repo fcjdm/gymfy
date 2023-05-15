059e5a2cd59ee909dc576e743a8c2d76c7508350
fcjdm <fcjdom@gmail.com>
2023-05-15 19:23:35 +0200

    Pantalla ejercicios

--- App.js
+++ App.js
@@ -2,16 +2,16 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './components/LoginScreen';
-import SuccessScreen from './components/SuccessScreen';
+import ExerciseScreen from './components/ExerciseScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Excercise">
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="SuccessScreen" component={SuccessScreen} />
+        <Stack.Screen name="Excercise" component={ExerciseScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ components/ExerciseScreen.js
@@ -0,0 +1,150 @@
+import React, { useState } from "react";
+import { View, Text, ScrollView, TouchableOpacity, Modal, StyleSheet, TextInput, Button } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { signOut } from 'firebase/auth';
+import { auth } from '../firebaseConfig';
+
+export default function SuccessScreen() {
+  // Ejemplos de datos de ejercicios
+  const exercises = [
+    { name: 'Ejercicio 1', duration: '10 minutos', difficulty: 'Fácil' },
+    { name: 'Ejercicio 2', duration: '15 minutos', difficulty: 'Medio' },
+    { name: 'Ejercicio 3', duration: '20 minutos', difficulty: 'Difícil' },
+  ];
+
+  const [userLists, setUserLists] = useState([
+    { id: 1, name: 'Lista 1' },
+    { id: 2, name: 'Lista 2' },
+    { id: 3, name: 'Lista 3' },
+  ]);
+
+  const [selectedExercise, setSelectedExercise] = useState(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [showUserLists, setShowUserLists] = useState(false);
+
+  const handleExerciseClick = (exercise) => {
+    setSelectedExercise(exercise);
+    setIsModalVisible(true);
+  };
+
+  const handleAddToList = () => {
+    // Lógica para agregar el ejercicio a una lista existente o crear una nueva lista
+    // Puedes utilizar las funciones de Firebase Firestore para realizar estas operaciones
+  };
+
+  const handleCreateNewList = () => {
+    // Lógica para crear una nueva lista y agregar el ejercicio a ella
+    // Puedes utilizar las funciones de Firebase Firestore para realizar estas operaciones
+    console.log('Crear una nueva lista y agregar el ejercicio a ella');
+  };
+
+  return (
+    <View>
+       <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 10 }}>Lista de Ejercicios</Text>
+      <ScrollView>
+        {exercises.map((exercise, index) => (
+          <TouchableOpacity key={index} onPress={() => handleExerciseClick(exercise)}>
+              <View style={{ padding: 10, borderBottomWidth: 1, borderBottomColor: '#ccc' }}>
+              <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{exercise.name}</Text>
+              <Text>Duración: {exercise.duration}</Text>
+              <Text>Dificultad: {exercise.difficulty}</Text>
+              </View>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+
+      <Modal visible={isModalVisible} transparent={true} animationType="fade">
+      <View style={styles.modalContainer}>
+        {!showUserLists ? (
+          <View style={styles.modalContent}>
+            <TouchableOpacity style={styles.closeButton} onPress={() => setIsModalVisible(false)}>
+              <Ionicons name="close-outline" size={24} color="black" />
+            </TouchableOpacity>
+
+            {selectedExercise && (
+              <View>
+                <Text style={styles.modalTitle}>{selectedExercise.name}</Text>
+                <Text style={styles.modalText}>Descripción: {selectedExercise.description}</Text>
+                <Text style={styles.modalText}>Dificultad: {selectedExercise.difficulty}</Text>
+                <Text style={styles.modalText}>Duración: {selectedExercise.duration}</Text>
+
+                <Button title="Añadir a una lista" onPress={() => setShowUserLists(true)} />
+              </View>
+            )}
+          </View>
+        ) : (
+          <View style={styles.modalContent}>
+            <TouchableOpacity style={styles.goBackButton} onPress={() => setShowUserLists(false)}>
+              <Ionicons name="arrow-back-outline" size={24} color="black" />
+            </TouchableOpacity>
+
+            <Text style={styles.addToListText}>Añadir a una lista:</Text>
+            <ScrollView style={styles.listContainer}>
+              {userLists.map((list) => (
+                <TouchableOpacity key={list.id} style={styles.listItem} onPress={() => handleAddToList(list.id)}>
+                  <Text>{list.name}</Text>
+                </TouchableOpacity>
+              ))}
+            </ScrollView>
+
+            <Button title="Crear nueva lista" onPress={handleCreateNewList} />
+          </View>
+        )}
+      </View>
+    </Modal>
+  </View>
+)};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    elevation: 5,
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  modalText: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  addToListText: {
+    fontSize: 16,
+    marginTop: 10,
+    marginBottom: 5,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+
+  closeButton: {
+    position: 'absolute',
+    top: 2,
+    right: 10,
+    zIndex: 1,
+    padding: 10,
+  },
+  goBackButton: {
+    position: 'absolute',
+    top: 2,
+    left: 10,
+    zIndex: 1,
+    padding: 10,
+  },
+});
